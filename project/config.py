cb900d5604ff546b8d84b2238e2130fa80a96376
cameronthecoder <camnooten@gmail.com>
2021-01-20 13:52:29 -0600

    Atempt to fix db

--- project/config.py
+++ project/config.py
@@ -24,7 +24,7 @@ class Config(object):
                 self.POSTGRES_DATABASE_NAME
             )
         else:
-            return os.environ['DATABASE_URL'] + "?sslmode=require"
+            return os.environ['DATABASE_URL'] + "?ssl=require"
 
 
 class ProductionConfig(Config):
