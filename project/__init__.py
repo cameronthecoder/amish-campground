fbf2ad4f468abb5882d312206009d6787995e348
cameronthecoder <camnooten@gmail.com>
2021-01-20 17:41:52 -0600

    attempt to fix ssl for the one hundreth time

--- project/__init__.py
+++ project/__init__.py
@@ -22,7 +22,7 @@ def create_app(testing=False):
     app.config.from_object(cfg)
 
 
-    database = Database(app.config['DATABASE_URI'])
+    database = Database(app.config['DATABASE_URI'], ssl=True)
 
     @app.before_serving
     async def create_db_pool():
