7c2e80328761dfd9c472aeb10b2d11661e9e257e
cameronthecoder <camnooten@gmail.com>
2021-01-20 21:40:04 -0600

    Fix db pool (hopefully) and remove db url

--- project/config.py
+++ project/config.py
@@ -10,25 +10,19 @@ class Config(object):
     POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
     POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
     POSTGRES_DATABASE_NAME = os.environ.get('POSTGRES_DATABASE_NAME')
-    SSL_ENABLED = False
 
 
     @property
     def DATABASE_URI(self):
-        if os.environ.get("DATABASE_URL") == None:
-            return 'postgresql://%s:%s@%s:%s/%s' % (
-                self.POSTGRES_USER, 
-                self.POSTGRES_PASSWORD, 
-                self.POSTGRES_HOST,
-                str(self.POSTGRES_PORT), 
-                self.POSTGRES_DATABASE_NAME
-            )
-        else:
-            print(os.environ['DATABASE_URL'] + "?sslmode=require")
-            return os.environ['DATABASE_URL'] + "?min_size=0&max_size=1"
+        return 'postgresql://%s:%s@%s:%s/%s' + "?min_size=1&max_size=4" % (
+            self.POSTGRES_USER, 
+            self.POSTGRES_PASSWORD, 
+            self.POSTGRES_HOST,
+            str(self.POSTGRES_PORT), 
+            self.POSTGRES_DATABASE_NAME
+        )
 
 class ProductionConfig(Config):
-    SSL_ENABLED = True
     DEBUG = True
 
 class DevelopmentConfig(Config):
