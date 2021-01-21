5c54d52e449ac350e53c11d4503eb8f928af3094
cameronthecoder <camnooten@gmail.com>
2021-01-20 20:37:03 -0600

    db checks

--- project/__init__.py
+++ project/__init__.py
@@ -20,36 +20,40 @@ def create_app(testing=False):
     else:
         cfg = import_string('project.config.ProductionConfig')()
     app.config.from_object(cfg)
-
-
+    
     database = Database(app.config['DATABASE_URI'])
 
     @app.before_serving
     async def create_db_pool():
-        await database.connect()
-        app.db = database
-        await app.db.execute("""
-        CREATE TABLE IF NOT EXISTS "site" 
-            (id SERIAL NOT NULL PRIMARY KEY,
-            name CHAR(30) NOT NULL
+        try:
+            await database.connect()
+        except Exception as e:
+            print(e)
+        if database.is_connected:
+            app.db = database
+            await app.db.execute("""
+            CREATE TABLE IF NOT EXISTS "site" 
+                (id SERIAL NOT NULL PRIMARY KEY,
+                name CHAR(30) NOT NULL
+                )
+            """)
+            await app.db.execute("""
+            CREATE TABLE IF NOT EXISTS "reservation" (
+                id SERIAL NOT NULL PRIMARY KEY,
+                first_name CHAR(60) NOT NULL,
+                last_name CHAR(60) NOT NULL,
+                start_date DATE NOT NULL,
+                end_date DATE NOT NULL,
+                site_id	INTEGER,
+                FOREIGN KEY(site_id) REFERENCES site(id)
             )
-        """)
-        await app.db.execute("""
-        CREATE TABLE IF NOT EXISTS "reservation" (
-            id SERIAL NOT NULL PRIMARY KEY,
-            first_name CHAR(60) NOT NULL,
-            last_name CHAR(60) NOT NULL,
-            start_date DATE NOT NULL,
-            end_date DATE NOT NULL,
-            site_id	INTEGER,
-            FOREIGN KEY(site_id) REFERENCES site(id)
-        )
-        """)
-        # GET db table information from SHOW CREATE TABLE
+            """)
+            # GET db table information from SHOW CREATE TABLE
 
     @app.after_serving
     async def disconnect_db():
-        await database.disconnect()
+        if database.is_connected:
+            await database.disconnect()
 
     from project.views import view
     from project.api import api
